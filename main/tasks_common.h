5dcccea480c75ee2e6f60b7489c4d8d14206d8cc
Simeon Georgiev <simeon.georgiev@m2mservices.com>
2024-11-12 22:00:32 +0200

    Initial commit

---
+++ main/tasks_common.h
@@ -0,0 +1,16 @@
+/*
+ * tasks_common.h
+ *
+ *  Created on: Nov 12, 2024
+ *      Author: sgeorgiev
+ */
+
+#ifndef MAIN_TASKS_COMMON_H_
+#define MAIN_TASKS_COMMON_H_
+
+// WiFi application task
+#define WIFI_APP_TASK_STACK_SIZE			4096
+#define WIFI_APP_TASK_PRIORITY				5
+#define WIFI_APP_TASK_CORE_ID				0
+
+#endif /* MAIN_TASKS_COMMON_H_ */
